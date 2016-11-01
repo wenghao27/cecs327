42a7e02324ac06fd723296119aab6172175c142c
wenghao27 <wenghao27@yahoo.com>
2016-10-31 23:34:48 -0700

    modify

--- StringArrayGenerationRunnable.java
+++ StringArrayGenerationRunnable.java
@@ -20,15 +20,11 @@ public class StringArrayGenerationRunnable  implements Runnable{
 	private long mTotalReplaceWaitTime = 0;
 	//this arrayList will store operations of searching waiting time in nanoseconds
 	private ArrayList<Long> mSearchWaitTime;
-	//this arrayList will store operations of searching waiting time in nanoseconds
+	//this arrayList will store operations of replacing and searching waiting time in nanoseconds
 	private ArrayList<Long> mReplaceWaitTime;
-
 	
 	private Lock mArrayLocks[];
 	
-	public ArrayList<Long> getSearchWaitTime() {
-		return mSearchWaitTime;
-	}
 	
 	public StringArrayGenerationRunnable(StringArrayGeneration s, int threadNum, int count) {
 		mStrArrays = s;
@@ -38,24 +34,6 @@ public class StringArrayGenerationRunnable  implements Runnable{
 		mReplaceWaitTime = new ArrayList<>();
 		mArrayLocks = new ReentrantLock[s.getPoolLength()];
 
-	//total waiting time of a thread
-	private long mTotalWaitTime = 0;
-	//this array will store waiting time of each operation in nanoseconds
-	private long[] mWaitTimeInNano;
-
-	private Lock mArrayLocks[];
-	private Lock currentLock;
-
-	public StringArrayGenerationRunnable(StringArrayGeneration s, int threadNum, int count) {
-
-		mStrArrays = s;
-		mRepetitions = count;
-		mThreadNum = threadNum;
-		mWaitTimeInNano = new long[count];
-		mArrayLocks = new ReentrantLock[s.getPoolLength()];
-
-
-	}
 
 	@Override
 
@@ -110,50 +88,10 @@ public class StringArrayGenerationRunnable  implements Runnable{
 							" nanoseconds \n" + "Average Standard Deviation: " + Math.round(SD_Repalce_Time) + " nanoseconds \n");
 					
 				}
-							
-				
-									
+								
 		}							
 
-	public void run(){
-
-		System.out.println("Start thread number: " + mThreadNum);
-
-		for(int i = 0; i < mRepetitions; i++){
-
-			int operation = getOperationNumber();
-
-
-			//System.out.println("Thread #" + mThreadNum + " with operation "  + i  + " waiting " + duration + "nanoseconds");
-
-
-			if(operation < 2000){
-				long time = mStrArrays.searchAndCount();
-				mWaitTimeInNano[i] = time;
-				mTotalWaitTime += time;
-				//System.out.println("count: " + count + " ");
-			}
-			else {
-				long time = mStrArrays.searchAndReplace();
-				mWaitTimeInNano[i] = time;
-				mTotalWaitTime += time;
-			}
-		}
-			//if a thread finish all the operations, calculate the average and standard deviation
-		System.out.println("total time: " + mTotalWaitTime);
-		long average = mTotalWaitTime / mRepetitions;
-		double standardDeviation = compute_standard_deviation(mWaitTimeInNano, average, mRepetitions);
-		//for(int j = 0; j < mRepetitions; j++){
-			//System.out.println("Thread " + mThreadNum + " " + mWaitTimeInNano[j] + "nanoseconds");
-		//}
-		System.out.println("Thread " + mThreadNum + " Average waiting time is: " + average);
-		System.out.println("Thread " + mThreadNum +  " Standard deviation is : " + standardDeviation);
-
-
-
-
-	}
-
+	
 	//get operation number randomly
 	public int getOperationNumber() {
 		Random rand = new Random();
